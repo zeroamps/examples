7b14d39fe5a20e98ad32a25f60fa16d5e364a001
Jozef Chovan <jozef.chovan@gmail.com>
2022-06-30 22:04:44 +0200

    Add firestore

--- mean/14_google-firebase/src/app/firestore/firestore.component.ts
+++ mean/14_google-firebase/src/app/firestore/firestore.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+export interface Student {
+  firstName: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-firestore',
   templateUrl: './firestore.component.html'
 })
 export class FirestoreComponent {
-  constructor(private firestore: AngularFirestore) {}
+  students: Observable<Student[]>;
+  constructor(private firestore: AngularFirestore) {
+    this.students = this.firestore.collection<Student>('students').valueChanges();
+  }
 }
